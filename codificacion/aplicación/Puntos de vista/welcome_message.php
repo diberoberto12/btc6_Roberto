6f8a625e81f6e28fa68a0a29dfc73f19ae0cfbb2
diberoberto12 <83965084+diberoberto12@users.noreply.github.com>
2021-05-13 09:57:19 -0500

    Cambiando el mensaje vista

--- codificacion/aplicación/Puntos de vista/welcome_message.php
+++ codificacion/aplicación/Puntos de vista/welcome_message.php
@@ -218,8 +218,8 @@
 	</div>
 
 	<div class="heroe">
-
-		<h1>Welcome to CodeIgniter <?= CodeIgniter\CodeIgniter::CI_VERSION ?></h1>
+                <h1>Roberto Leon Alcantara</h
+		<h1>Bienbenidos a mi aplicacion hecha en:Codeigniter  <?= CodeIgniter\CodeIgniter::CI_VERSION ?></h1>
 
 		<h2>The small framework with powerful features</h2>
 
