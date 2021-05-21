68be3d3db5a9cf3935ac7418ffe96c17be94d2e9
diberoberto12 <83965084+diberoberto12@users.noreply.github.com>
2021-05-21 11:24:31 -0500

    Update success.php

--- codeingniter/app/Views/success.php
+++ codeingniter/app/Views/success.php
@@ -5,16 +5,9 @@
 <div class="container">
     <div class="row text-center">
         <div class="col-sm-6 col-sm-offset-3">
-        <br><br> <h2 style="color:#0fad00">Success</h2>
+        <br><br> <h1 style="color:#0fad00">{user_name}</h2>
+        <br><br> <h2 style="color:#0fad00">{password}</h2> 
         </div>
     </div>
 </body>
 </html>
-
-    <?php
-    
-    echo $mydata1;
-    
-    echo"<br>" $mydata2; 
-    
-    ?>
